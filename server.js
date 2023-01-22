88df867bc7530d036006d25ce21f0fbce1a04efa
Durgavino <mailtodurga93@gmail.com>
2023-01-22 00:04:40 -0400

    update port

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const fs=require('fs');
+
+const app = express();
+const db = require('./Develop/db/db.json');
+
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(db)
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'notes.html/'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+// app.listen(3000,function () {
+//     console.log('app running');
+// });
