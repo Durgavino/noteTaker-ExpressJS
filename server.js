f59e97711c73ec6eecd46c757d5a4cd769a1564f
Durgavino <mailtodurga93@gmail.com>
2023-01-22 01:20:55 -0400

    updating Post

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const path = require('path');
 const fs=require('fs');
 const app = express();
+const bodyParser=require('body-parser');
 const db=require('./db/db.json');
+// const { title } = require('process');
+// const { text } = require('body-parser');
 const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// var urlencodedparser=bodyParser.urlencoded({extended:false});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,6 +29,21 @@ app.get('*', (req, res) => {
 app.get('/api/notes', (req, res) => {
   res.json(db)
 });
+
+app.post('/api/notes',(req,res)=>{
+  // response={
+  //   title:req.body.title,
+  //   text:req.body.text
+  // }
+  // console.log(response);
+  // res.end(Json.stringify(response));
+  //res.json(db)
+
+  res.json(`${req.method} request recieved`);
+ 
+});
+
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
